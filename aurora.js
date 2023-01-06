4c39ae8a751af409d8e234eb2f9ad76bd46ac552
Typonomy <lqconnor@alaska.edu>
2023-01-05 23:20:31 -0500

    Add files via upload

---
+++ aurora.js
@@ -0,0 +1,3 @@
+document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Typonomy/RealTwitterBlue-AuroraEdition/main/aurora.jpg')";
+document.body.style.backgroundAttachment = "fixed";
+document.body.style.backgroundSize= "cover";
